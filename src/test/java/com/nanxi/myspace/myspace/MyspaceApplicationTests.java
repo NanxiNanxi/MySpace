caf0391f30c84cddde6ef18d07a353e0b37ee497
NanxiNanxi <sunbowpig@gmail.com>
2020-01-09 13:02:44 -0600

    Create a reporsitory for sprint boot project my space

---
+++ src/test/java/com/nanxi/myspace/myspace/MyspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanxi.myspace.myspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
